c55628d568fd4f3f4aaed5277f57b38b34d87af4
Kyrian <kyrian.arvene@gmail.com>
2025-01-15 02:02:29 +0100

    Ajout du repository AuthRepository.java

---
+++ backend/src/main/java/com/urbanhoney/backend/repository/AuthRepository.java
@@ -0,0 +1,27 @@
+package com.urbanhoney.backend.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.urbanhoney.backend.models.UserEntity;
+import com.urbanhoney.backend.usecase.dto.UserDto;
+
+
+@Repository
+public interface AuthRepository extends JpaRepository<UserEntity, Long> {
+    
+    Optional<UserEntity> findById(Integer id);
+
+    Optional<UserEntity> findByEmail(String email);
+
+    Optional<UserEntity> findByUsername(String username);
+
+    @Query("SELECT u FROM UserEntity u WHERE u.email = :emailOrUsername OR u.username = :emailOrUsername")
+    Optional<UserEntity> findByEmailOrUsername(@Param("emailOrUsername") String emailOrUsername);
+
+    void save(UserDto userDto);
+}
