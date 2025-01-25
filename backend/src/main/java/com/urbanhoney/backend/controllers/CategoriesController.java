01eb732afbf02fb75424ee91eb64a4b30bbfe017
Kyrian <kyrian.arvene@gmail.com>
2025-01-25 22:04:13 +0100

    Categorie controller suppression d'un  import non utilisé

--- backend/src/main/java/com/urbanhoney/backend/controllers/CategoriesController.java
+++ backend/src/main/java/com/urbanhoney/backend/controllers/CategoriesController.java
@@ -9,7 +9,6 @@ import com.urbanhoney.backend.usecase.dto.request.AddSubCategorieRequestDto;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
