ab121b9f61f98e2cfd9c018464ff71ac60daea01
Kyrian <kyrian.arvene@gmail.com>
2025-01-27 16:23:46 +0100

    Amélioration de categories controller

--- backend/src/main/java/com/urbanhoney/backend/usecase/dto/request/AddCategorieRequestDto.java
+++ backend/src/main/java/com/urbanhoney/backend/usecase/dto/request/AddCategorieRequestDto.java
@@ -1,11 +1,19 @@
 package com.urbanhoney.backend.usecase.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class AddCategorieRequestDto {
+    @NotEmpty(message = "Please enter a categorie name")
+    @Max(value = 15, message = "Your categorie name must not excede 15 characters")
     private String categorie;
+
+    @NotEmpty(message = "please enter a gender for the categorie")
+    @Pattern(regexp = "^(H|F|H/F)$", message = "Gender must be 'H', 'F', or 'H/F'")
     private String gender;
 }
--- backend/src/main/java/com/urbanhoney/backend/usecase/dto/request/AddSubCategorieRequestDto.java
+++ backend/src/main/java/com/urbanhoney/backend/usecase/dto/request/AddSubCategorieRequestDto.java
@@ -2,13 +2,23 @@ package com.urbanhoney.backend.usecase.dto.request;
 
 import com.urbanhoney.backend.models.CategorieEntity;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class AddSubCategorieRequestDto {
+
+    @NotEmpty(message = "Please enter a sub categorie name")
+    @Max(value = 15, message = "Your sub categorie name must not excede 15 characters")
     private String subCategorieName;
+
     private CategorieEntity categorieLinkId;
+
+    @NotEmpty(message = "please enter a gender for the categorie")
+    @Pattern(regexp = "^(H|F|H/F)$", message = "Gender must be 'H', 'F', or 'H/F'")
     private String gender;
 }
