8310d0c9d3b55a871aca8eebd8d8b81ea2917230
Kyrian <kyrian.arvene@gmail.com>
2025-02-23 00:56:09 +0100

    Articles page

---
+++ urbanhoney_Front-end/src/app/pages/articles-page/articles-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-articles-page',
+  imports: [],
+  templateUrl: './articles-page.component.html',
+  styleUrl: './articles-page.component.scss'
+})
+export class ArticlesPageComponent {
+
+}
